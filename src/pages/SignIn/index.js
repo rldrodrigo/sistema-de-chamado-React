18c785ef241e4231132e258c4be88fed2152f343
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-10-15 15:15:13 -0300

    <feat> Tela de login

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,10 +1,36 @@
+import { useState } from 'react';
+import './signin.css';
+import logo from '../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
 function SignIn() {
-    return (
-      <div>
-        <h1>Página de login</h1>
-      </div>
-    );
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e){
+    e.preventDefault();
+    alert('Clicou');
   }
+
+  return (
+    <div className="container-center">
+      <div className="login">
+        <div className="logo-area">
+          <img src={logo} alt="Logo do Sistema" />
+        </div>
+
+        <form onSubmit={handleSubmit}>
+            <h1> Entrar </h1>
+            <input type="text" placeholder="email@email.com" value={email} onChange={ (e)=> setEmail(e.target.value)} />
+            <input type="password" placeholder="*********" value={password} onChange={ (e)=> setPassword(e.target.value)}  />
+            <button type="submit">Acessar</button>
+        </form>
+
+        <Link to="/register">Criar uma conta </Link>
+      </div>
+    </div>
+  );
+}
   
-  export default SignIn;
+export default SignIn;
   
