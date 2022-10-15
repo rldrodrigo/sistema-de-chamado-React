e317f29cc09000a0b7f5e4d7fb39c002bfbf078b
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-10-15 11:46:13 -0300

    <feat> Configuração das rotas

---
+++ src/pages/SignIn/index.js
@@ -0,0 +1,10 @@
+function SignIn() {
+    return (
+      <div>
+        <h1>Página de login</h1>
+      </div>
+    );
+  }
+  
+  export default SignIn;
+  
---
+++ src/pages/SignUp/index.js
@@ -0,0 +1,10 @@
+function SignUp() {
+    return (
+      <div>
+        <h1>Página de Cadastro</h1>
+      </div>
+    );
+  }
+  
+  export default SignUp;
+  
