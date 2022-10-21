294bcdc98d2cabbb050258ebfc83d957487c9bc5
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-10-20 22:24:35 -0300

    <feat> Configuração do header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,34 @@
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
+
+import './header.css';
+import { AuthContext } from '../../contexts/auth';
+import avatar from '../../assets/avatar.png';
+
+import { FiHome, FiUser, FiSettings } from "react-icons/fi";
+
+export default function Header(){
+
+    const { user } = useContext(AuthContext);
+
+    return (
+        <div className="sidebar">
+            <div>
+                <img src={user.avatarUrl ? user.avatarUrl : avatar} alt="Foto avatar" />
+            </div>
+
+            <Link to="/dashboard">
+                <FiHome color="#FFF" size={24}/>
+                Chamados
+            </Link>
+            <Link to="/dashboard">
+                <FiUser color="#FFF" size={24}/>
+                Clients
+            </Link>
+            <Link to="/dashboard">
+                <FiSettings color="#FFF" size={24}/>
+                Configurações
+            </Link>
+        </div>
+    )
+}
