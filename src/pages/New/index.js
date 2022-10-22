d35d19db528926eebc7a9bc618b1920fbbdea583
unknown <rodrigo.lima1945@gmail.com>
2022-10-22 01:43:38 -0300

    <feat> configuração do formulario

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -2,12 +2,25 @@ import Title from '../../components/Title';
 import Header from '../../components/Header';
 import './new.css';
 import { FiPlusCircle } from 'react-icons/fi';
+import { useState } from 'react';
 
 export default function New() {
+
+    const [assunto, setAssunto] = useState("Suporte");
+    const [status, setStatus] = useState("Aberto");
+    const [complemento, setComplemento] = useState("")
 
     function handleRegister(e) {
         e.preventDefault();
 
+    }
+
+    function handleChangeSelect(e) {
+        setAssunto(e.target.value);
+    }
+
+    function handleOptionChange(e){
+        setStatus(e.target.value);
     }
 
     return (
@@ -27,7 +40,7 @@ export default function New() {
                         </select>
 
                         <label>Assunto:</label>
-                        <select>
+                        <select value={assunto} onChange={handleChangeSelect}>
                             <option value="Suporte">Primeira opção</option>
                             <option value="Visita Tecnica">Visita Técnica</option>
                             <option value="Financeiro">Financeiro</option>
@@ -36,18 +49,18 @@ export default function New() {
 
                         <label>Assunto:</label>
                         <div className="status">
-                            <input type="radio" name="radio" value="Aberto" />
+                            <input type="radio" name="radio" value="Aberto" onChange={handleOptionChange} checked={ status === "Aberto"} />
                             <span>Em Aberto </span>
 
-                            <input type="radio" name="radio" value="Aberto" />
+                            <input type="radio" name="radio" value="Progresso" onChange={handleOptionChange} checked={ status === "Progresso"} />
                             <span>Progresso </span>
 
-                            <input type="radio" name="radio" value="Aberto" />
+                            <input type="radio" name="radio" value="Atendido" onChange={handleOptionChange} checked={ status === "Atendido"} />
                             <span>Atendido</span>
                         </div>
 
                         <label>Complemento</label>
-                        <textarea type="text" placeholder="Descreva seu problema (opcional)." />
+                        <textarea type="text" placeholder="Descreva seu problema (opcional)." value={complemento} onChange={(e) => setComplemento(e.target.value)} />
 
                         <button type="submit"> Salvar </button>
                     </form>
