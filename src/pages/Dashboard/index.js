0a8c5fcb4091d8141804a7efcf2809672b0e3144
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2022-10-23 12:44:19 -0300

    <feat> Buscando os chamados e armazenando em uma lista

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,15 +1,76 @@
-import { useContext, useState } from "react"
+import { useContext, useState, useEffect } from "react"
 import { AuthContext } from '../../contexts/auth';
 
 import './dashboard.css';
 
+import firebase from '../../services/firebaseConection';
 import Header from "../../components/Header";
 import Title from "../../components/Title";
 import { FiEdit2, FiMessageSquare, FiPlus, FiSearch } from "react-icons/fi";
 import { Link } from "react-router-dom";
+import { format } from "date-fns";
+
+const listRef = firebase.firestore().collection('chamados').orderBy('created', 'desc');
 
 export default function Dashboard() {
-    const [chamados, setChamados] = useState([1]);
+    const [chamados, setChamados] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [loadingMore, setLoadingMore] = useState(false);
+    const [isEmpty, setIsEmpty] = useState(false);
+    const [lastDocs, setLastDocs] = useState();
+
+    useEffect(() => {
+
+        loadChamados();
+
+        return () => {
+
+        }
+    }, []);
+
+    async function loadChamados() {
+        await listRef.limit(5)
+        .get()
+        .then((snapshot) => {
+            updateState(snapshot);
+        })
+        .catch((err) => {
+            console.log(err);
+            setLoadingMore(false);
+        })
+        
+        setLoading(false);
+    }
+
+    async function updateState(snapshot) {
+        const isCollectionEmpty = snapshot.size === 0;
+
+        if(!isCollectionEmpty) {
+            let lista = [];
+
+            snapshot.forEach((doc) => {
+                lista.push({
+                    id: doc.id,
+                    assunto: doc.data().assunto,
+                    cliente: doc.data().cliente,
+                    clienteId: doc.data().clienteId,
+                    created: doc.data().created,
+                    createdFormated: format(doc.data().created.toDate(), 'dd/MM/yyyy'),
+                    status: doc.data().status,
+                    complemento: doc.data().complemento,
+                })
+            })
+
+            const lastDoc = snapshot.docs[snapshot.docs.length - 1]; //Pega o ultimo documento buscado
+
+            setChamados(chamados => [ ...chamados, ...lista]);
+            setLastDocs(lastDoc);
+        } else {
+            setIsEmpty(true);
+        }
+
+        setLoadingMore(false);
+    }
 
     return (
         <div>
